92367cb31637928dcbcdc6fd88fa4ea07981eda2
hippargisr <hippargisunil@gmail.com>
2020-05-19 01:11:12 +0530

    if search not found in rendering

--- my_app/views.py
+++ my_app/views.py
@@ -21,31 +21,35 @@ def new_search(request):
 
 	post_list = soup.find_all('li', {'class': 'result-row'})
 
-	post_title = post_list[0].find(class_ = 'result-title').text
-	post_url = post_list[0].find('a').get('href')
-	# post_price = post_list[0].find(class_ = 'result-price').text
-	# print(post_title,post_url)
-	final_posting = []
-	for post in post_list:
-		post_title = post.find(class_ = 'result-title').text
-		post_url = post.find('a').get('href')
+	if post_list:
+		post_title = post_list[0].find(class_ = 'result-title').text
+		post_url = post_list[0].find('a').get('href')
+		# post_price = post_list[0].find(class_ = 'result-price').text
+		# print(post_title,post_url)
+		final_posting = []
+		for post in post_list:
+			post_title = post.find(class_ = 'result-title').text
+			post_url = post.find('a').get('href')
 
-		if post_list[0].find(class_ = 'result-price'):
-			post_price = post.find(class_='result-price').text
-		else:
-			post_price = 'N/A'
+			if post_list[0].find(class_ = 'result-price'):
+				post_price = post.find(class_='result-price').text
+			else:
+				post_price = 'N/A'
 
-		if post.find(class_ = 'result-image').get('data-ids'):
-			post_image_id = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
-			post_image_url = BASE_IMAGE_URL.format(post_image_id)
-			print('image--->',post_image_url)
-		else:
-			post_image_url = 'https://craigslist.org/images/peace.jpg'
+			if post.find(class_ = 'result-image').get('data-ids'):
+				post_image_id = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
+				post_image_url = BASE_IMAGE_URL.format(post_image_id)
+				print('image--->',post_image_url)
+			else:
+				post_image_url = 'https://craigslist.org/images/peace.jpg'
 
-		final_posting.append((post_title, post_url, post_price, post_image_url))
+			final_posting.append((post_title, post_url, post_price, post_image_url))
+	else:
+		error_message = "You didn't select a choice.",
 
 	context = {
 		'search': search,
 		'final_posting': final_posting,
+		'error_message': error_message,
 	}
 	return render(request, 'my_app/new_search.html', context)
